68759ba6a53a00740b35a6606599b9862134f9f0
SGTeam <xsgteamx@gmail.com>
2026-07-26 23:19:07 +0800

    复习上次的列表的内容，学习python中函数的内容 对比JAVA

---
+++ anan/01-Python基础/day03/test01.py
@@ -0,0 +1,10 @@
+#row = 2 #行号为2
+#while row > 0 : #行号
+#         print("*" * 3)
+#         row -= 1
+def testPrintStar():
+    row = 2 #行号为2
+    while row > 0 : #行号
+            print("*" * 3)
+            row -= 1
+testPrintStar()
